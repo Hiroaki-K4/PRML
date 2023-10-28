87d52cc0a62adfd688be526691821d231cbf3434
Hiroaki-K4 <49no19@gmail.com>
2023-10-28 09:05:03 +0900

    Update ci

--- test.sh
+++ test.sh
@@ -16,20 +16,40 @@ test_probability_distribution() {
     cd probability_distribution
     python3 draw_3d_trigonometric_graph.py NotShow
     check_result "draw_3d_trigonometric_graph.py"
-    python3 draw_beta_distribution.py
-    check_result "draw_beta_distribution.py"
-    python3 draw_binomial_distribution.py
-    check_result "draw_binomial_distribution.py"
     python3 draw_contour_map.py NotShow
     check_result "draw_contour_map.py"
-    python3 draw_dirichlet_distribution.py
-    check_result "draw_dirichlet_distribution.py"
     python3 draw_trigonometric_graph.py NotShow
     check_result "draw_trigonometric_graph.py"
-    python3 draw_gaussian_distribution.py
-    check_result "draw_gaussian_distribution.py"
     python3 draw_multi_dimentional_gaussian_distribution.py NotShow
     check_result "draw_multi_dimentional_gaussian_distribution.py"
+    cd ../
+}
+
+test_draw_beta_distribution() {
+    cd probability_distribution
+    python3 draw_beta_distribution.py
+    check_result "draw_beta_distribution.py"
+    cd ../
+}
+
+test_draw_binomial_distribution() {
+    cd probability_distribution
+    python3 draw_binomial_distribution.py
+    check_result "draw_binomial_distribution.py"
+    cd ../
+}
+
+test_draw_dirichlet_distribution() {
+    cd probability_distribution
+    python3 draw_dirichlet_distribution.py
+    check_result "draw_dirichlet_distribution.py"
+    cd ../
+}
+
+test_draw_gaussian_distribution() {
+    cd probability_distribution
+    python3 draw_gaussian_distribution.py
+    check_result "draw_gaussian_distribution.py"
     cd ../
 }
 
@@ -38,8 +58,14 @@ python3 -m pip install -r requirements.txt
 if [ $# -eq 1 ]; then
     if [ $1 = "probability_distribution" ]; then
         test_probability_distribution
-    # elif [ $1 = "elliptic_fitting_by_fns" ]; then
-        # test_elliptic_fitting_by_fns
+    elif [ $1 = "draw_beta_distribution" ]; then
+        test_draw_beta_distribution
+    elif [ $1 = "draw_binomial_distribution" ]; then
+        test_draw_binomial_distribution
+    elif [ $1 = "draw_dirichlet_distribution" ]; then
+        test_draw_dirichlet_distribution
+    elif [ $1 = "draw_gaussian_distribution" ]; then
+        test_draw_gaussian_distribution
     else
         echo "Argument is wrong"
         exit 1
@@ -47,4 +73,8 @@ if [ $# -eq 1 ]; then
 
 else
     test_probability_distribution
+    test_draw_beta_distribution
+    test_draw_binomial_distribution
+    test_draw_dirichlet_distribution
+    test_draw_gaussian_distribution
 fi
