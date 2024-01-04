ec301e920aa15fea44bfe97659c54d44b9f1d3da
Hiroaki-K4 <49no19@gmail.com>
2024-01-05 08:48:20 +0900

    Update vias variance decomposition

--- linear_regression_model/draw_bias_variance_decomposition.py
+++ linear_regression_model/draw_bias_variance_decomposition.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def calculate_gauss(x, mu, sigma=5):
+def calculate_gauss(x, mu, sigma=1.5):
     return np.exp(-((x - mu) ** 2) / 2 * sigma**2)
 
 
@@ -15,24 +15,9 @@ def calculate_gauss_mu(data_num, idx, range_start, range_end):
     return mu
 
 
-def update_weights(x, y, dim):
-    A = np.zeros((dim + 1, dim + 1))
-    T = np.zeros(dim + 1)
-    for i in range(dim + 1):
-        for j in range(dim + 1):
-            multi_x = np.array(x) ** (i + j)
-            A[i, j] = sum(multi_x)
-        T[i] = np.dot(np.array(x) ** i, np.array(y))
-
-    return np.dot(np.linalg.inv(A), T)
-
-
-def predict(W, x):
-    pred = np.zeros(x.shape)
-    for i in range(len(W)):
-        pred += np.dot(W[i], x**i)
-
-    return pred
+def update_weights(design_mat, y, lam):
+    W = np.dot(np.dot(np.linalg.inv(np.dot(lam, np.identity(int(y.shape[0]))) + np.dot(design_mat.T, design_mat)), design_mat.T), y)
+    return W
 
 
 def calculate_design_matrix(noise_x, degree, range_start, range_end):
@@ -47,7 +32,7 @@ def calculate_design_matrix(noise_x, degree, range_start, range_end):
     return design_mat
 
 
-def predict_gauss(W, x, range_start, range_end):
+def predict(W, x, range_start, range_end):
     pred = np.zeros(x.shape)
     for i in range(len(W)):
         if i == 0:
@@ -55,46 +40,52 @@ def predict_gauss(W, x, range_start, range_end):
         else:
             mu = calculate_gauss_mu(len(W)-1, i-1, range_start, range_end)
             pred += np.dot(W[i], calculate_gauss(x, mu))
-            print(calculate_gauss(x, mu).shape)
 
     return pred
 
+
 def main():
+    fig = plt.figure(figsize=(16, 9))
+    lam_1_graph = fig.add_subplot(121)
+    lam_1_avg_graph = fig.add_subplot(122)
+    lam_2_graph = fig.add_subplot(221)
+    lam_2_avg_graph = fig.add_subplot(222)
+
     # Create dataset
     random.seed(0)
     x = np.linspace(0, 2 * np.pi, 500)
     y = np.sin(x)
-    range_start = -1
-    range_end = 1
+    range_start = -5
+    range_end = 5
     N = 25
     L = 100
     pred_sum = np.zeros(x.shape)
     for i in range(L):
         nums = random.sample(range(x.shape[0]), k=N)
-        # print(nums)
         noise_x = []
         noise_y = []
         for idx in nums:
             random_x = x[idx]
-            random_y = y[idx] + np.random.normal(0, 0.2)
+            random_y = y[idx] + np.random.normal(0, 0.01)
             noise_x.append(random_x)
             noise_y.append(random_y)
 
-        degree = 3
-        W = update_weights(np.array(noise_x), np.array(noise_y), degree)
-        pred = predict(W, np.array(x))
-        pred_sum += pred
-        
+        degree = 24
         design_mat = calculate_design_matrix(np.array(noise_x), degree+1, range_start, range_end)
-        print(design_mat)
-        predict_gauss(W, np.array(x), range_start, range_end)
-        input()
+        W = update_weights(design_mat, np.array(noise_y), -0.31)
+        # W = update_weights(design_mat, np.array(noise_y), 2.6)
+        # W = update_weights(design_mat, np.array(noise_y), -2.4)
+        pred = predict(W, np.array(x), range_start, range_end)
+        pred_sum += pred
+        lam_1_graph.plot(x, pred, label="Prediction", c="green")
+
 
     pred_avg = pred_sum / L
+    # pred_avg = pred_sum
 
-
-    plt.plot(x, y, label="True")
-    plt.plot(x, pred_avg, label="Prediction")
+    lam_1_graph.plot(x, y, label="True", c="red")
+    plt.plot(x, y, label="True", c="red")
+    # plt.plot(x, pred_avg, label="Prediction")
     plt.scatter(noise_x, noise_y, label="Noise points")
     plt.legend()
     title = "Polynomial curve fitting(degree={0},N={1})".format(str(degree), str(N))
