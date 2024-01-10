5610b21f0f3d52fbd72de0212bf9eccd03ae62fc
Hiroaki-K4 <49no19@gmail.com>
2024-01-11 08:50:11 +0900

    Add create_data_space func

--- linear_regression_model/draw_sequential_bayesian_learning.py
+++ linear_regression_model/draw_sequential_bayesian_learning.py
@@ -5,63 +5,68 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 sys.path.append("../")
-from probability_distribution import draw_gaussian_distribution
+from probability_distribution import draw_multivariate_gaussian_distribution
 
 
-def calculate_multivariate_gaussian_distribution(mu, cov, vals):
-    if mu.shape[0] != cov.shape[0] or mu.shape[0] != cov.shape[1]:
-        raise ValueError("Shape of mu and cov are not same.")
+def create_data_space(rand_w, x_min, x_max):
+    w_0 = rand_w[:, 0]
+    w_1 = rand_w[:, 1]
+    print(w_0)
+    print(w_1)
+    print(rand_w)
+    for i in range(w_0.shape[0]):
+        y_0 = w_0[i] * x_min + w_1[i]
+        y_1 = w_0[i] * x_max + w_1[i]
+        x_0 = x_min
+        x_1 = x_max
+        print(y_0)
+        print(y_1)
+        input()
+        if y_0 > 1:
+            y_0 = 1
+            x_0 = (y_0 - w_1[i]) / w_0[i]
+        elif y_0 < 0:
+            y_0 = 0
+            x_0 = (y_0 - w_1[i]) / w_0[i]
+        if y_1 > 1:
+            y_1 = 1
+            x_1 = (y_1 - w_1[i]) / w_0[i]
+        elif y_1 < 0:
+            y_1 = 0
+            x_1 = (y_1 - w_1[i]) / w_0[i]
 
-    density = np.empty((vals.shape[0], vals.shape[1]))
-    for i in range(vals.shape[0]):
-        for j in range(vals.shape[1]):
-            density[i][j] = (
-                (1 / (2 * np.pi) ** (mu.shape[0] / 2))
-                * (1 / np.linalg.det(cov) ** 0.5)
-                * np.exp(
-                    -1
-                    / 2
-                    * np.dot(
-                        np.dot(np.transpose(vals[i][j] - mu), np.linalg.inv(cov)),
-                        (vals[i][j] - mu),
-                    )
-                )
-            )
-
-    return density
-
-
-def calculate_conditional_gaussian_distribution(mu, cov, y):
-    pre_mat = np.linalg.inv(cov)
-    var_a = pre_mat[0, 0] ** (-1)
-    mu_a = mu[0] - pre_mat[0, 0] ** (-1) * pre_mat[0, 1] * (y - mu[1])
-
-    return mu_a, var_a
 
 
 def main():
+    np.random.seed(314)
+
+    fig = plt.figure()
+    pri_post_dist = fig.add_subplot(121)
+    data_space = fig.add_subplot(122)
+
     N = 100
-    X = np.linspace(-1, 1, N)
-    Y = np.linspace(-1, 1, N)
-    X, Y = np.meshgrid(X, Y)
+    w_0 = np.linspace(-1, 1, N)
+    w_1 = np.linspace(-1, 1, N)
+    w_0, w_1 = np.meshgrid(w_0, w_1)
 
     mu = np.array([0, 0])
     cov = np.array([[0.2, 0.0], [0.0, 0.2]])
 
-    vals = np.empty(X.shape + (2,))
-    vals[:, :, 0] = X
-    vals[:, :, 1] = Y
+    vals = np.empty(w_0.shape + (2,))
+    vals[:, :, 0] = w_0
+    vals[:, :, 1] = w_1
 
-    density = calculate_multivariate_gaussian_distribution(mu, cov, vals)
+    density = draw_multivariate_gaussian_distribution.calculate_multivariate_gaussian_distribution(mu, cov, vals)
 
-    fig = plt.figure()
-    cnf = fig.add_subplot(121)
-    cond_dist = fig.add_subplot(122)
+    rand_w = np.random.multivariate_normal(mu, cov, size=6)
+    print(rand_w)
 
-    cnf.contourf(X, Y, density, alpha=0.8)
-    cnf.set_title("Multivariate Gaussian distribution")
-    cnf.set_xlabel("$w_0$")
-    cnf.set_ylabel("$w_1$")
+    create_data_space(rand_w, data_space, -1, 1)
+
+    pri_post_dist.contourf(w_0, w_1, density, alpha=0.8)
+    pri_post_dist.set_title("Prior/Posterior distribution")
+    pri_post_dist.set_xlabel("$w_0$")
+    pri_post_dist.set_ylabel("$w_1$")
 
 
 
