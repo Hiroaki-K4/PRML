922bab3cfef0120de3a61bc2550598e9ee3fa62c
Hiroaki-K4 <49no19@gmail.com>
2024-02-10 09:43:01 +0900

    Update model evidence

--- linear_regression_model/evaluate_evidence_function.py
+++ linear_regression_model/evaluate_evidence_function.py
@@ -1,3 +1,4 @@
+import math
 import random
 import sys
 
@@ -5,53 +6,67 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def update_weights(x, y, dim):
-    A = np.zeros((dim + 1, dim + 1))
-    T = np.zeros(dim + 1)
-    for i in range(dim + 1):
-        for j in range(dim + 1):
-            multi_x = np.array(x) ** (i + j)
-            A[i, j] = sum(multi_x)
-        T[i] = np.dot(np.array(x) ** i, np.array(y))
+def calculate_design_matrix_using_polynomial(x, degree):
+    design_mat = np.ones((len(x), degree))
+    for col in range(degree):
+        design_mat[:, col] = np.array(x) ** col
 
-    return np.dot(np.linalg.inv(A), T)
+    return design_mat
 
 
-def predict(W, x):
-    pred = np.zeros(x.shape)
-    for i in range(len(W)):
-        pred += np.dot(W[i], x**i)
+def calculate_evidence_function(x, y, degree, alpha, beta):
+    y_arr = np.array(y)
+    design_mat = calculate_design_matrix_using_polynomial(x, degree)
+    M = degree - 1
+    N = y_arr.shape[0]
+    A = alpha * np.identity(degree) + beta * np.dot(design_mat.T, design_mat)
+    m_N = beta * np.dot(np.dot(np.linalg.inv(A), design_mat.T), y_arr)
+    E_m_N = beta / 2 * np.linalg.norm(
+        y_arr - np.dot(design_mat, m_N)
+    ) ** 2 + alpha / 2 * np.dot(m_N.T, m_N)
+    evidence_func = (
+        M / 2 * math.log(alpha)
+        + N / 2 * math.log(beta)
+        - E_m_N
+        - 1 / 2 * math.log(np.linalg.det(A))
+        - N / 2 * math.log(2 * math.pi)
+    )
 
-    return pred
+    return evidence_func
 
 
 def main():
     # Create dataset
-    random.seed(0)
+    random.seed(314)
+    np.random.seed(314)
     x = np.linspace(0, 2 * np.pi, 500)
     y = np.sin(x)
     N = 30
     nums = random.sample(range(x.shape[0]), k=N)
+    noise_std = 0.2
     noise_x = []
     noise_y = []
     for idx in nums:
         random_x = x[idx]
-        random_y = y[idx] + np.random.normal(0, 0.2)
+        random_y = y[idx] + np.random.normal(0, noise_std)
         noise_x.append(random_x)
         noise_y.append(random_y)
 
-    degree = 8
-    W = update_weights(np.array(noise_x), np.array(noise_y), degree)
-    pred = predict(W, np.array(x))
+    beta = (1 / 0.2) ** 2
+    alpha = 0.005
+    Ms = []
+    evi = []
+    for degree in range(1, 10):
+        evidence_func = calculate_evidence_function(
+            noise_x, noise_y, degree, alpha, beta
+        )
+        Ms.append(degree - 1)
+        evi.append(evidence_func)
 
-    plt.plot(x, y, label="True")
-    plt.plot(x, pred, label="Prediction")
-    plt.scatter(noise_x, noise_y, label="Noise points")
-    plt.legend()
-    title = "Polynomial curve fitting(degree={0},N={1})".format(str(degree), str(N))
-    plt.title(title)
-
-    # TODO: Crate design matrix of polynomial
+    plt.xlabel("$M$")
+    plt.ylabel("log evidence function")
+    plt.title("Relationship between dimetion $M$ of polynomial and model evidence")
+    plt.plot(Ms, evi)
 
 
 if __name__ == "__main__":
