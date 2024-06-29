308d8c48b431c82999d875632a4afe7b0bac706e
Hiroaki-K4 <49no19@gmail.com>
2024-06-29 15:38:14 +0900

    Plot gaussian process

--- kernel_method/draw_gaussian_process.py
+++ kernel_method/draw_gaussian_process.py
@@ -3,7 +3,6 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import norm
 
 
 def calculate_kernel(x_0, x_1, params):
@@ -27,18 +26,16 @@ def calculate_kernel_for_cov(x, params):
 
 
 def predict_by_gaussian_process(x_train, y_train, x_test, std):
-    value = std**2
     diago_elems = np.full(x_train.shape[0], std**2)
     C_var = np.diag(diago_elems)
 
     params = [1, 4, 0, 0]
     mean = np.zeros(len(x_test))
     var = np.zeros(len(x_test))
-    beta = 1 / std
     for i in range(x_test.shape[0]):
         k = calculate_kernel(x_train, x_test[i], params)
         C = calculate_kernel_for_cov(x_train, params) + C_var
-        c = calculate_kernel(x_test[i], x_test[i], params) + beta
+        c = calculate_kernel(x_test[i], x_test[i], params) + std
         mean[i] = np.dot(np.dot(k.T, np.linalg.inv(C)), y_train)
         var[i] = c - np.dot(np.dot(k.T, np.linalg.inv(C)), k)
 
@@ -50,9 +47,8 @@ def main():
     random.seed(0)
     x = np.linspace(0, 2 * np.pi, 300)
     y = np.sin(x)
-    N = 20
-    nums = random.sample(range(x.shape[0]), k=N)
-    nums.sort()
+    N = 50
+    nums = random.sample(range(int(x.shape[0] - x.shape[0] / 3)), k=N)
     noise_x = []
     noise_y = []
     std = 0.1
@@ -63,20 +59,25 @@ def main():
         noise_x.append(random_x)
         noise_y.append(random_y)
 
-    del_num = 8
-    noise_x = noise_x[: N - del_num]
-    noise_y = noise_y[: N - del_num]
-
     mean, var = predict_by_gaussian_process(
         np.array(noise_x), np.array(noise_y), np.array(x), std
     )
+    max_y_list = []
+    min_y_list = []
+    for i in range(var.shape[0]):
+        max_y = mean[i] + np.sqrt(abs(var[i])) * 2
+        min_y = mean[i] - np.sqrt(abs(var[i])) * 2
+        max_y_list.append(max_y)
+        min_y_list.append(min_y)
 
-    # TODO: Plot variance
     plt.plot(x, y, label="True", c="green")
     plt.plot(x, mean, label="Prediction", c="red")
+    plt.fill_between(
+        x, min_y_list, max_y_list, label=r"std $\times 2$", color="orange", alpha=0.8
+    )
     plt.scatter(noise_x, noise_y, label="inputs")
     plt.legend()
-    title = "Regression by Gaussian process(N={0})".format(str(N - del_num))
+    title = "Regression by Gaussian process(N={0})".format(str(N))
     plt.title(title)
 
 
