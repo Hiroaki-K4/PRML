8728b79f26b0233c77c2a01e1bb69dc52ab6a6cc
Hiroaki-K4 <49no19@gmail.com>
2024-08-11 17:47:49 -0700

    Update quadratic programming

--- kernel_machine/svm.py
+++ kernel_machine/svm.py
@@ -1,16 +1,46 @@
 import sys
-import matplotlib.pyplot as plt
 
+import cvxopt
+import matplotlib.pyplot as plt
+import numpy as np
 from sklearn import svm
 from sklearn.datasets import make_blobs
 from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.model_selection import train_test_split
+
+
+def calculate_gaussian_kernel(x_0, x_1, param=0.45):
+    return np.exp(-param * np.linalg.norm(x_0 - x_1) ** 2)
+
+
+def create_kernel_matrix(Xs):
+    k = np.zeros((Xs.shape[0], Xs.shape[0]))
+    for i in range(Xs.shape[0]):
+        for j in range(Xs.shape[0]):
+            k[i][j] = calculate_gaussian_kernel(Xs[i], Xs[j])
+
+    return k
+
+
+def calculate_quadratic_programming(X_train, y_train):
+    print(X_train)
+    # Calculate parameters(P, q, G, h, A, b)
+    y = np.outer(y_train, y_train)
+    print(y)
+    print(y.shape)
+    k = create_kernel_matrix(X_train)
+    P = cvxopt.matrix(np.outer(y_train, y_train) * k)
+    print(P)
+    # TODO: Calculate q
 
 
 def main():
     # we create 40 separable points
     X, y = make_blobs(n_samples=50, centers=2, random_state=6)
-    print("X: ", X)
-    print("y: ", y)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    calculate_quadratic_programming(X_train, y_train)
     input()
 
     # fit the model, don't regularize for illustration purposes
@@ -18,6 +48,8 @@ def main():
     clf.fit(X, y)
 
     plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Paired)
+    print(X)
+    print(y)
 
     # plot the decision function
     ax = plt.gca()
